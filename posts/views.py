72e0aa33f35d7dc8801b90c18e2be5dd4d82e0b1
bumbum94 <tong940526@gmail.com>
2019-11-03 20:16:38 +0900

    내 코드

---
+++ posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .forms import PostForm
+from .models import Post
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.htm', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/form.htm', context)
